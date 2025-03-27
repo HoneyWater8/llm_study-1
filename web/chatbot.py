5dce08de67f7837370af399429a1f285df8b2009
HoneyWater8 <kjyjyh8@gmail.com>
2025-03-27 15:36:43 +0900

    app함수로 묶음.

--- web/chatbot.py
+++ web/chatbot.py
@@ -7,29 +7,32 @@ from common.llm.call_provider import get_response_from_llm, PROVIDER_TYPE
 from common.screen.constant import ROLE_TYPE
 from common.screen.utils import init_page, init_display
 
-st.title("Chatbot")
-# 페이지 초기화 
-init_page() 
+def app():
+  st.title("Chatbot")
+  # 페이지 초기화 
+  init_page() 
 
-# 화면 초기화  
-choiced_provider, choiced_llm = init_display()
-# 사용자 입력
-prompt = get_prompt()
+  # 화면 초기화  
+  choiced_provider, choiced_llm = init_display()
+  # 사용자 입력
+  prompt = get_prompt()
 
-if prompt is not None:
-  # 사용자 메시지를 세션 상태에 추가
-  add_history(ROLE_TYPE.user, prompt)
-  # 사용자 메시지 표시
-  print_message(ROLE_TYPE.user.name, prompt)
-  
-  # AI 응답 요청
-  generator = get_response_from_llm(
-      choiced_provider=PROVIDER_TYPE[choiced_provider]
-      , messages=st.session_state.messages, llm_name=choiced_llm)
-  # AI 응답 표시
-  assistant_message = print_message(
-    ROLE_TYPE.assistant.name
-    , generator)
-  
-  add_history(ROLE_TYPE.assistant, assistant_message)
+  if prompt is not None:
+    # 사용자 메시지를 세션 상태에 추가
+    add_history(ROLE_TYPE.user, prompt)
+    # 사용자 메시지 표시
+    print_message(ROLE_TYPE.user.name, prompt)
+    
+    # AI 응답 요청
+    generator = get_response_from_llm(
+        choiced_provider=PROVIDER_TYPE[choiced_provider]
+        , messages=st.session_state.messages, llm_name=choiced_llm)
+    # AI 응답 표시
+    assistant_message = print_message(
+      ROLE_TYPE.assistant.name
+      , generator)
+    
+    add_history(ROLE_TYPE.assistant, assistant_message)
 
+if __name__ == "__main__":
+  app()
