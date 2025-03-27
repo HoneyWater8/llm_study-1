a42439b459c4b1159f5dab36b86daa2b0c0f6a00
good593 <goodwon593@gmail.com>
2025-03-27 16:45:52 +0900

    클래스 적용 완료

--- web/common/llm/call_provider.py
+++ web/common/llm/call_provider.py
@@ -1,31 +1,29 @@
 import enum
 import time
-from openai import OpenAI
-from groq import Groq
 
-from common.llm.openai import OPENAI_LLMs, ClientOpenAI
-from common.llm.llama import GROQ_LLMs, ClientGROQ
-from common.llm.ollama import OLLAMA_LLMs, ClientOllama
+from common.llm.provider import Provider
+from common.llm.openai import OPENAI_LLMs, Provider_OPENAI
+from common.llm.llama import GROQ_LLMs, Provider_GROQ
+from common.llm.ollama import OLLAMA_LLMs, Provider_OLLAMA
 
 class PROVIDER_TYPE(enum.Enum):
   # 제공자명 = (인덱스, 호출함수, 사용가능한 모델 리스트)
-  groq = (enum.auto(), ClientGROQ, GROQ_LLMs)
-  openai = (enum.auto(), ClientOpenAI, OPENAI_LLMs)
-  ollama = (enum.auto(), ClientOllama, OLLAMA_LLMs)
+  groq = (enum.auto(), Provider_GROQ, GROQ_LLMs)
+  openai = (enum.auto(), Provider_OPENAI, OPENAI_LLMs)
+  ollama = (enum.auto(), Provider_OLLAMA, OLLAMA_LLMs)
 
-
-def get_response_from_llm(choiced_provider:PROVIDER_TYPE, messages, llm_name:str):
-  if not isinstance(choiced_provider, PROVIDER_TYPE):
+def get_response_from_llm(choiced_provider:Provider, messages, llm_name:str):
+  if not isinstance(choiced_provider, Provider):
     raise Exception("허락한 제공자가 아닙니다.") 
-  elif llm_name not in choiced_provider.value[2].__members__:
+  elif llm_name not in choiced_provider.provider_LLMs.__members__:
     raise Exception("허락한 모델명이 아닙니다.") 
 
-  generator = choiced_provider.value[1](
+  generator = choiced_provider(
     model_name=llm_name, messages=messages
   )
   for token in generator:
     yield token
-    time.sleep(0.05) 
+    time.sleep(0.05)
 
 
 
--- web/common/llm/llama.py
+++ web/common/llm/llama.py
@@ -1,24 +1,26 @@
 import enum
 import time
 from groq import Groq
+from .provider import Provider
 
 class GROQ_LLMs(enum.Enum):
   llama3 = (enum.auto(), "llama-3.3-70b-versatile") 
   qwen = (enum.auto(), "qwen-qwq-32b")
 
-def ClientGROQ(
-    model_name, messages:list[dict], stream=True
-):
-  
-  client = Groq()
-  response = client.chat.completions.create(
-    model=GROQ_LLMs[model_name].value[1],
-    messages=messages,
-    stream=stream
-  )
+class Provider_GROQ(Provider):
+  def __init__(self, provider_LLMs=GROQ_LLMs) -> None:
+    super().__init__(provider_LLMs)
+    self.client = Groq()
 
-  for token in response:
-    if token.choices[0].delta.content is not None:
-      yield token.choices[0].delta.content
-      time.sleep(0.05) 
+  def __call__(self, 
+      model_name, messages):
+    response = self.client.chat.completions.create(
+      model=self.provider_LLMs[model_name].value[1],
+      messages=messages,
+      stream=True
+    )
 
+    for token in response:
+      if token.choices[0].delta.content is not None:
+        yield token.choices[0].delta.content
+        time.sleep(0.05) 
