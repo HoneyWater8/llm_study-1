213412957cd244d2664e88a0bcc6b7f8bfc80a7a
good593 <goodwon593@gmail.com>
2025-03-31 15:07:38 +0900

    service 추가

--- web/pages/db.py
+++ web/pages/db.py
@@ -1,28 +1,10 @@
 import streamlit as st 
-import os
+from common.service.film_service import excution_by_sql
 
-# create connection
-conn = st.connection(
-  name="my_db",
-  type="sql",
-  dialect="postgresql",
-  host=os.getenv("host"),
-  port=os.getenv("port"),
-  database=os.getenv("database"),
-  username=os.getenv("uname"),
-  password=os.getenv("password")
-)
+########################################################
+# 화면 영역
+########################################################
+df_data, title_data = excution_by_sql()
 
-sql = """
-select
-  f1.title
-  , count(f2.actor_id) as actor_cnt
-from film f1
-left join film_actor f2 
-	on f1.film_id = f2.film_id
-group by f1.film_id
-;
-"""
-
-df = conn.query(sql, ttl="10m")
-st.dataframe(df)
+st.title(title_data)
+st.dataframe(df_data)
